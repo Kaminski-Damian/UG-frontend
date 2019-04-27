dd260415d9cba5bee643f076fd74686a88fb6108
Kaminski-Damian <kaminski.damian.gdynia@gmail.com>
2019-04-27 18:41:32 +0200

    lab5: koniec zadania

--- lab5/src/script.js
+++ lab5/src/script.js
@@ -44,4 +44,73 @@ class App {
     }
     this.response = null
   };
+
+  addNewTruck (newTruck) {
+    let alreadyExistingTruck;
+    if (!!this.database.records.length) {
+      alreadyExistingTruck = this.database.records
+        .find(truck => truck.regNumber === newTruck.regNumber);
+    }
+    if (!!alreadyExistingTruck) {
+      return this.sendResponse('Truck already exist in database', null);
+    }
+    newTruck.id = ++this.database.currentId;
+    this.database.records.push(newTruck);
+  
+    return this.sendResponse('OK', null);
+  }
+
+  getAllTrucks() {
+    return this.sendResponse('OK', this.database.records);
+  }
+
+  findTruckById(id) {
+    const searchedTruck = this.database.records
+      .find(truck => truck.id === id);
+    const msg = !!searchedTruck ? 'OK' : 'Upsss!';
+    return this.sendResponse(msg, searchedTruck);
+  }
+
+  updateTruck(id, params) {
+    const record = JSON.parse(this.findTruckById(id));
+    if (record.msg !== 'OK') {
+      return this.sendResponse(record.msg, null);
+    } 
+    for (const param in params) {
+      if(params.hasOwnProperty(param)) {
+        if (param === 'id') {
+          return this.sendResponse('Upsss!', null); 
+        }
+        record.data[param] = params[param];
+      }
+    };
+    this.database.records = this.database.records
+      .map(truck => {
+        if (truck.id === record.data.id) {
+          truck = record.data;
+        }
+        return truck;
+      });
+    return this.sendResponse('OK', null);
+  }
+
+  deleteTruck(id) {
+    const deletedTruckIndex = this.database.records
+      .findIndex(truck => truck.id === id);
+    if (deletedTruckIndex < 0) {
+      return this.sendResponse('Upsss!', null); 
+    }
+    this.database.records.splice(deletedTruckIndex, 1);
+    return this.sendResponse('OK', null); 
+  }
+
+  prepareResponse(msg, data) {
+    this.response = { msg, data };
+    return JSON.stringify(response);
+  }
+
+  sendResponse(msg, data) {
+    this.response = this.prepareResponse(msg, data);
+    return this.response;
+  }
 }
