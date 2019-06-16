177d8404c8e9b82546cffc03ad6bf4b432e360c1
Kaminski-Damian <kaminski.damian.gdynia@gmail.com>
2019-06-16 08:06:35 +0200

    projekt: podłączono back do front

--- React/src/App.js
+++ React/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 import AddDriver from './components/AddDriver';
 import AddTruck from './components/AddTruck';
@@ -8,24 +9,79 @@ import Trucks from './components/Trucks';
 class App extends Component {
   state = {
     trucks: [],
-    drivers: [
-      { id: 1, name: 'Janusz', yearOfBirth: 1410 },
-      { id: 2, name: 'Marian', yearOfBirth: 1999 },
-    ]
+    drivers: [],
+    fracht: []
+  }
+
+  componentDidMount() {
+    this.getDrivers();
+    this.getTrucks();
+  }
+
+  updateFracht() {
+    this.state.trucks.forEach((truck, index) => {
+      const driver = this.state.drivers
+        .find(driver => driver.id === truck.driverId);
+      if (!!driver) {
+        this.setState({
+          fracht: [ ...this.state.fracht, { ...truck, driver } ]
+        });
+      }
+    });
+    this.updatedTrucks();
+  }
+
+  updatedTrucks() {
+    this.setState({
+      trucks: []
+    });
+  }
+
+  getDrivers() {
+    axios.get('http://localhost:4000/api/drivers')
+      .then(res => res.data)
+      .then(drivers => {
+        this.setState({
+          drivers: [ ...this.state.drivers, ...drivers ]
+        });
+      })
+      .catch(err => console.error(err));
+  }
+
+  getTrucks() {
+    axios.get('http://localhost:4000/api/trucks')
+      .then(res => res.data)
+      .then(trucks => {
+        this.setState({
+          trucks: [ ...this.state.trucks, ...trucks ]
+        });
+        this.updateFracht();
+      })
+      .catch(err => console.error(err));
   }
 
   addDriver = (newDriver) => {
-    newDriver.id = Math.random();
-    this.setState({
-      drivers: [ ...this.state.drivers, newDriver ]
-    });
+    axios.post('http://localhost:4000/api/drivers', newDriver)
+      .then(() => {
+        this.setState({
+          drivers: [ ...this.state.drivers, newDriver ]
+        });
+        this.updateFracht();
+      })
+      .catch(err => console.error(err))
   }
 
   addTruck = (newTruck) => {
-    newTruck.id = Math.random();
-    this.setState({
-      trucks: [ ...this.state.trucks, newTruck ]
-    });
+    newTruck = { ...newTruck, driverId: newTruck.driver.id };
+    delete newTruck.driver;
+    axios.post('http://localhost:4000/api/trucks', newTruck)
+      .then(() => {
+        this.setState({
+          trucks: [ ...this.state.trucks, newTruck ]
+        });
+        this.updateFracht();
+      })
+      .catch(err => console.error(err));
   }
 
   render() {
@@ -34,7 +90,7 @@ class App extends Component {
         <AddDriver addDriver={ this.addDriver } />
         <AddTruck addTruck={ this.addTruck } drivers={ this.state.drivers } />
         <hr />
-        <Trucks trucks={ this.state.trucks } />
+        <Trucks trucks={ this.state.fracht } />
       </div>
     );
   }
